407e950d5d90bb247401089999c57a8eb9f0eb4e
Olivier Amrein <oenvoyage@gmail.com>
2022-01-29 21:10:43 +0100

    adding a Vapor Explosion

--- environment/environment.gd
+++ environment/environment.gd
@@ -6,6 +6,7 @@ export var a = -1
 # var b = "text"
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,6 +16,17 @@ func _ready():
 func _process(delta):
 	pass
 	#$Animationtree["parameters/blend_position"] = 1
+
+func _input(ev):
+	if Input.is_key_pressed(KEY_K):
+		var playback = $AnimationTree2.get("parameters/playback")
+		playback.travel("Water_chill")
+		print("KKKK")
+	if Input.is_key_pressed(KEY_L):
+		var playback = $AnimationTree2.get("parameters/playback")
+		playback.travel("Fire_burning")
+		$AnimationTree2.set("")
+		print("LLLL")
 
 func switch_side():
 	print("switched sides in ENVIRONEMNT")
